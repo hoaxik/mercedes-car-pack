bf72f3d721bc916f8bf631e80451a92be50c7284
HoaX <81192082+hoaxik@users.noreply.github.com>
2022-09-11 03:51:42 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,8 @@ game 'gta5'
 
 author 'HoaX#8692'
 description 'Resource with Mercedes Cars'
-repository 'https://github.com/PLOKMJNB/FiveM-Civ-Car-Pack'
+repository 'https://github.com/hoaxik/mercedes-car-pack'
+version '1.0'
 
 files {
     'data/**/*.meta',
@@ -20,9 +21,5 @@ data_file 'CARCOLS_FILE'            'data/**/carcols*.meta'
 data_file 'VEHICLE_VARIATION_FILE'    'data/**/carvariations*.meta'
 data_file 'CONTENT_UNLOCKING_META_FILE' 'data/**/*unlocks.meta'
 data_file 'PTFXASSETINFO_FILE' 'data/**/ptfxassetinfo.meta'
-
-client_scripts {
-    'vehicle_names.lua',
-}
 
 server_script 'server.lua'
